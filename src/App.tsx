0633024b16642adcdd9be40129561584eb76f9eb
浜田　俊祐 <hamashun0820@icloud.com>
2023-08-08 22:29:57 +0900

    divタグの大まかな配置を決めた

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { board, body, controller, game, info, next } from './app.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className={body}>
+            <div className={game}>
+                <div className={board}></div>
+                <div>
+                    <div className={next}></div>
+                    <div className={controller}></div>
+                </div>
+            </div>
+            <div className={info}></div>
+        </div>
+    );
 }
 
-export default App
+export default App;
