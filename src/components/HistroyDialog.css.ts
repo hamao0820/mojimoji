128d7888aa984fb25dbb5bd469c9bc1a6ab14691
浜田　俊祐 <hamashun0820@icloud.com>
2023-08-14 22:26:58 +0900

    💄 単語一覧のダイアログのcssを調整

--- src/components/HistroyDialog.css.ts
+++ src/components/HistroyDialog.css.ts
@@ -5,12 +5,16 @@ export const dialog = style({
     width: '50vw',
     height: '50vh',
     borderRadius: '10px',
+    overflow: 'hidden',
 });
 
 export const dialogContent = style({
     width: '100%',
     height: '100%',
     borderRadius: '10px',
-    display: 'gird',
-    placeItems: 'center',
+    overflow: 'auto',
+    padding: '5px 0 5px 5px',
+    fontSize: '1.2rem',
+    fontWeight: 'bold',
+    fontFamily: 'ヒラギノ丸ゴ Pro',
 });
